99d3475be5e6dddd8742f1b48621b2777929a72a
Tatiane <30663303+tatianepinto@users.noreply.github.com>
2022-03-08 09:13:06 -0500

    get tag from query

--- routes/posts.js
+++ routes/posts.js
@@ -5,13 +5,22 @@ const data = require("../db/forTestPurpose.json");
 
 /* GET posts and params from url */
 router.get('/', function (req, res, next) {
-    const tags = req.query;
-    console.log(tags, " - tags");
-    const result = data.posts.map(postUnit => {
-        postUnit.tags.map(tag => tag === tags);
-    });
-
-    res.render('posts', { answer: result });
+    const queryTags = req.query.tags.split(',');
+    if (queryTags !== '') {
+        console.log(queryTags, " - tags");
+        const result = { posts: [] };
+        data.posts.forEach((postUnit) => {
+            // console.log(postUnit, " - postUnit");
+            postUnit.tags.forEach((tag) => {
+                const index = queryTags.indexOf(tag);
+                if (index !== -1) { result.posts.push(postUnit); }
+            })
+        });
+        console.log(result, " - result");
+        console.log(result.posts.length, " - result.posts.length");
+        console.log(data.posts.length, " - data.length");
+        res.render('posts', { answer: JSON.stringify(result) });
+    } else { }
 });
 
 module.exports = router;
